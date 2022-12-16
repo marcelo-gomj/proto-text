3a10b3c33c63a59629ee3c9d4c5d100b5346e320
Marcelo <marcelo.gomj@gmail.com>
2022-12-16 18:28:10 -0300

    fix: form component incomplete

--- src/components/SingupUser/index.tsx
+++ src/components/SingupUser/index.tsx
@@ -9,78 +9,80 @@ interface FormValues {
 }
 
 export function FormSingup() {
-   const  {register, handleSubmit } = useForm<FormValues>();
+   // const  {register, handleSubmit } = useForm<FormValues>();
    const [ data, setData ] = useState("");
 
    function submitData(data: FormValues){
       console.log(data)
    }
 
-   return (
-      <form
-         onSubmit={handleSubmit(submitData)}
+   // return (
+   //    <form
+   //       onSubmit={handleSubmit(submitData)}
 
-      >
+   //    >
 
-         <label htmlFor="name">Nome Completo:
-            <input
-               {...register("name", {
-                  required: true,
-                  maxLength: 25,
-                  minLength: 3
-               })}
-               type="text"
-               id="name"
-            />
-         </label>
+   //       <label htmlFor="name">Nome Completo:
+   //          <input
+   //             {...register("name", {
+   //                required: true,
+   //                maxLength: 25,
+   //                minLength: 3
+   //             })}
+   //             type="text"
+   //             id="name"
+   //          />
+   //       </label>
 
-         <label htmlFor={"username"}>Nome do Usuário:
-            <input
-               {...register("username", {
-                  required: true,
-                  pattern: /^[\w-\.]+/g,
-                  maxLength: 15,
-                  minLength: 3
-               })}
+   //       <label htmlFor={"username"}>Nome do Usuário:
+   //          <input
+   //             {...register("username", {
+   //                required: true,
+   //                pattern: /^[\w-\.]+/g,
+   //                maxLength: 15,
+   //                minLength: 3
+   //             })}
 
-               type="text"
-               id="username"
-            />
-         </label>
+   //             type="text"
+   //             id="username"
+   //          />
+   //       </label>
 
-         <label htmlFor="email">Email:
-            <input
-               {...register("email", { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g })}
-               type="email"
-               id="email"
-            />
+   //       <label htmlFor="email">Email:
+   //          <input
+   //             {...register("email", { required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g })}
+   //             type="email"
+   //             id="email"
+   //          />
 
-         </label>
+   //       </label>
 
-         <label htmlFor="password">Digite uma senha:
-            <input
-               {...register("password", { required: true, minLength: 6 })}
-               type="password"
-               id="password"
-            />
+   //       <label htmlFor="password">Digite uma senha:
+   //          <input
+   //             {...register("password", { required: true, minLength: 6 })}
+   //             type="password"
+   //             id="password"
+   //          />
 
-         </label>
+   //       </label>
 
-         <label htmlFor="password">Digite a senha novamente:
-            <input
-               {...register("repeatPassword", { required: true, minLength: 6 })}
-               type="password"
-               id="repeatPassword"
-            />
+   //       <label htmlFor="password">Digite a senha novamente:
+   //          <input
+   //             {...register("repeatPassword", { required: true, minLength: 6 })}
+   //             type="password"
+   //             id="repeatPassword"
+   //          />
 
-         </label>
+   //       </label>
 
-         <input
-            type="submit"
-            value="Cadastrar"
-            className={cadastrar["submit-button"]}
-         />
+   //       <input
+   //          type="submit"
+   //          value="Cadastrar"
+   //          className={cadastrar["submit-button"]}
+   //       />
 
-      </form>
-   )
+   //    </form>
+   // )
+
+   return <></>
 }
